e7add5859b5ca8e0130b812222abfa752da60709
Jose Valdez Gaxiola <josevaldezgaxiola@gmail.com>
2023-02-10 14:00:57 -0700

    feat: add global fetcher with SWRConfig wrapper

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import { BrowserRouter } from "react-router-dom"
+import { SWRConfig } from "swr"
 
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <BrowserRouter>
-    <App />
+    <SWRConfig value={{ fetcher }}>
+      <App />
+    </SWRConfig>
   </BrowserRouter>
-);
-
+)
