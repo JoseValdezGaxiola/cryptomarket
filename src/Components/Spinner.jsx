920bcdd8a6262fd3e5a52babeecd00ca46ea5b4a
Jose Valdez Gaxiola <josevaldezgaxiola@gmail.com>
2023-03-22 18:58:50 -0700

    fix: correct propierty className

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -2,9 +2,9 @@ import React from "react"
 
 const Spinner = () => {
   return (
-    <div class='flex min-h-screen w-full items-center justify-center bg-gray-200'>
-      <div class='flex h-14 w-14 animate-spin items-center justify-center rounded-full bg-gradient-to-tr from-indigo-500 to-pink-500'>
-        <div class='h-9 w-9 rounded-full bg-gray-200'></div>
+    <div className='flex min-h-screen w-full items-center justify-center bg-gray-200'>
+      <div className='flex h-14 w-14 animate-spin items-center justify-center rounded-full bg-gradient-to-tr from-indigo-500 to-pink-500'>
+        <div className='h-9 w-9 rounded-full bg-gray-200'></div>
       </div>
     </div>
   )
